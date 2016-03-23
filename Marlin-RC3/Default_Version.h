8a552cae1356120ef74f1c3cb5fa4947016713b7
Tico06 <e.grammatico@gmail.com>
2016-03-23 19:23:27 +0100

    Time stamp de la compile ajouté

---
+++ Marlin-RC3/Default_Version.h
@@ -0,0 +1,15 @@
+/*
+ * This file is a placeholder for a file which could be distributed in an archive
+ * It takes the place of an automatically created "_Version.h" which is generated during the build process
+ */
+
+// #error "You must specify the following parameters related to your distribution"
+
+#if true
+//#define SHORT_BUILD_VERSION "1.1.0-RC3"
+#define SHORT_BUILD_VERSION __TIMESTAMP__
+#define DETAILED_BUILD_VERSION "1.1.0-RC3 From Archive"
+#define STRING_DISTRIBUTION_DATE __TIMESTAMP__
+// It might also be appropriate to define a location where additional information can be found
+#define SOURCE_CODE_URL  "http:// ..."
+#endif
